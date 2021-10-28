d38dc3ef5b7248ef572151aa5b354d23c117ecdc
Rudradeep Guha <rudradeep4@gmail.com>
2021-10-28 12:21:37 +0530

    Updates

--- src/MainImg.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Image } from '@chakra-ui/react';
-import mainImg from './me2.svg';
-
-export const MainImg = props => {
-  return <Image src={mainImg} h={300} w={300}/>;
-};
